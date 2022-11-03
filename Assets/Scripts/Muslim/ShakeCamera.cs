172153204989ef96f02835fa3d60382763ff7140
Orange13378 <muslim_askerli@mail.ru>
2022-11-04 00:33:16 +0300

    Fix Shake Range

--- Assets/Scripts/Muslim/ShakeCamera.cs
+++ Assets/Scripts/Muslim/ShakeCamera.cs
@@ -1,13 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShakeCamera : MonoBehaviour
 {
     [SerializeField] private float durationAfterRestart;
     [SerializeField] private float durationShake;
-    [SerializeField] private float cameraPosX;
-    [SerializeField] private float cameraPosY;
+    [SerializeField] private float startPosRange;
+    [SerializeField] private float endPosRange;
+    
+    
+    
+    private float cameraPosX;
+    private float cameraPosY;
     
     private Camera _camera;
     private Vector3 _originalPosition;
@@ -29,8 +33,8 @@ public class ShakeCamera : MonoBehaviour
 
         while ((timeLeft + durationShake) > Time.time)
         {
-            cameraPosX = Random.Range(-0.3f, 0.3f);
-            cameraPosY = Random.Range(-0.3f, 0.3f);
+            cameraPosX = Random.Range(startPosRange, endPosRange);
+            cameraPosY = Random.Range(startPosRange, endPosRange);
             _camera.transform.position = new Vector3(cameraPosX, cameraPosY, _originalPosition.z); 
             yield return new WaitForSeconds(durationAfterRestart);
         }
